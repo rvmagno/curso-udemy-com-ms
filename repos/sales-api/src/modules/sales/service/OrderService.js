049d6406868bad817feca647476f450a15f09203
rvmagno <rvmagno@gmail.com>
2024-06-02 00:14:30 -0300

    ate aula 57

--- repos/sales-api/src/modules/sales/service/OrderService.js
+++ repos/sales-api/src/modules/sales/service/OrderService.js
@@ -1,8 +1,9 @@
 import OrderRepository from "../repository/OrderRepository.js";
 import { sendMessageToProductStockUpdateQueue } from "../../product/rabbitmq/productStockUpdateSender.js";
 import { PENDING, ACCEPTED, REJECTED } from "../status/OrderStatus.js";
-import { OrderException } from "../exception/OrderException.js"
-import { BAD_REQUEST } from "../../../config/constants/httpStatus.js"
+import { OrderException } from "../exception/OrderException.js" ;
+import { BAD_REQUEST } from "../../../config/constants/httpStatus.js" ;
+import ProductClient  from "../../product/client/ProductClient.js";
 
 
 class OrderService {
@@ -14,20 +15,13 @@ class OrderService {
             this.validateOrderDate(orderData);
 
             const { authUser } = req;
-            let order = {
-                status: PENDING,
-                user: authUser,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                products: orderData,
-            };
+            const { authorization } = req.headers;
+            let order = createInitialOrderData(orderData, authUser);
 
-            
-
-            await this.validateProductStock(order);
+            await this.validateProductStock(order, authorization);
 
             let createdOrder  = await OrderRepository.save(order)
-            sendMessageToProductStockUpdateQueue(createdOrder.products);
+            this.sendMessage(createdOrder);
 
             return {
                 status : httpStatus.SUCCESS,
@@ -42,6 +36,16 @@ class OrderService {
         }
     }
 
+    createInitialOrderData(orderData, authUser){
+        return {
+            status: PENDING,
+            user: authUser,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            products: orderData,
+        };
+    }
+
     async updateOrder(orderMessage){
         try {
             const order = JSON.parse(orderMessage);
@@ -49,6 +53,7 @@ class OrderService {
                 let existingOrder = await OrderRepository.findById(order.salesId);
                 if(order.status && order.status !== existingOrder){
                     existingOrder.status = order.status;
+                    existingOrder.updatedAt = new Date();
                     await OrderRepository.save(existingOrder);
                 }
             }
@@ -67,8 +72,9 @@ class OrderService {
         }
     }
 
-    async validateProductStock(order){
-        let stockIsOut = true;
+    async validateProductStock(order, token){
+        let stockIsOut = await ProductClient.checkProducStock(order.products, token);
+        ProductClient
         if(stockIsOut){
             throw new OrderException(
                 BAD_REQUEST,
@@ -76,6 +82,13 @@ class OrderService {
             )
         }
 
+    }
+    sendMessage(createdOrder){
+        const message ={
+            salesId: createdOrder.id,
+            products: createdOrder.products,
+        }
+        sendMessageToProductStockUpdateQueue(message);
     }
 
 }
